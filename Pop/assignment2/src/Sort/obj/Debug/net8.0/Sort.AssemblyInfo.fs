07b1c40693b9a1100856b29bdbb32f9d2bc82db8
P1N60 <victor.guldregn@gmail.com>
2024-11-02 09:26:18 +0100

    No changes, just running dotnet build.

--- Pop/assignment2/src/Sort/obj/Debug/net8.0/Sort.AssemblyInfo.fs
+++ Pop/assignment2/src/Sort/obj/Debug/net8.0/Sort.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Sort")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d3c925f65b3f35f4bd528d926f659262f6c053d")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a3be2313d4bedd16f0d0e0280c6250a2d59ce6")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Sort")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Sort")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
