59c302853ea33da4f465babb4cc949ac2b80b433
Victor <victor.guldregn@gmail.com>
2024-11-07 15:54:52 +0100

    PoP 8-2

--- Pop/pop-8/pop-8-2/pop-8-2.fsx
+++ Pop/pop-8/pop-8-2/pop-8-2.fsx
@@ -1,6 +1,23 @@
 type ConsList<'a> = Nil | Cons of 'a * ConsList<'a>
 
-let rec toList (cl: ConsList<'a>): list = 
+// * a)
+let rec toList (cl: ConsList<'a>): 'a list = 
     match cl with
-    | Nil -> ()
-    | Cons (e, nl) -> [e] @ toList nl
+    | Nil -> []
+    | Cons (e, nl) -> e :: toList nl
+
+printfn "%A" (toList (Cons(true, Cons(false, Nil))))
+
+// * b)
+let genConsList (n: int): ConsList<int> = 
+    let mutable i = n
+    match n with
+    | 0 -> Nil
+    | _ ->
+        let mutable cl = Cons (i, Nil)
+        while i - 1 > 0 do
+            i <- i - 1
+            cl <- Cons (i, cl)
+        cl
+
+printfn "%A" (genConsList 5)
