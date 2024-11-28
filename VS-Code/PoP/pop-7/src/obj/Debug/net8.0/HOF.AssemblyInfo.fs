3a5f9a070d68bc35a627fdcbabc861a808b13e33
P1N60 <victor.guldregn@gmail.com>
2024-11-28 16:33:17 +0100

    Mac update

--- VS-Code/PoP/pop-7/src/obj/Debug/net8.0/HOF.AssemblyInfo.fs
+++ VS-Code/PoP/pop-7/src/obj/Debug/net8.0/HOF.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("HOF")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9bcadf1feeb4b5ff69b513ce1712d8468ba5c93")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca5605427a6cfb051eb6a6bc88e0a1f50609333")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("HOF")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("HOF")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- VS-Code/PoP/pop-8/pop-8-1/obj/Debug/net8.0/pop-8-1.AssemblyInfo.fs
+++ VS-Code/PoP/pop-8/pop-8-1/obj/Debug/net8.0/pop-8-1.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("pop-8-1")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9bcadf1feeb4b5ff69b513ce1712d8468ba5c93")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ca5605427a6cfb051eb6a6bc88e0a1f50609333")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("pop-8-1")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("pop-8-1")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
