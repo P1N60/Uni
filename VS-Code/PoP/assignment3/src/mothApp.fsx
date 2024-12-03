6bbc4d5c0127d17f0fb3532291fba10d13ce18ff
P1N60 <victor.guldregn@gmail.com>
2024-12-03 08:18:42 +0100

    Mac inden pop11-1

--- VS-Code/PoP/assignment3/src/mothApp.fsx
+++ VS-Code/PoP/assignment3/src/mothApp.fsx
@@ -10,7 +10,7 @@ open Color
 let w = 1400
 let h = w / 2
 
-type Moth(startPos: float * float) =
+type Moth(startPos: Vec) =
     
     let mothSpeed = 4.0
     let mutable currentPos = startPos // Renamed to avoid conflict
@@ -36,18 +36,32 @@ type Moth(startPos: float * float) =
             |> List.map (fun x -> pointPolar currentPos (radius, x))
         piecewiseAffine white 4.0 coords
 
-    member this.pos = currentPos // Expose current position
+    member this.pos = currentPos
+
+type Light = 
+    member this.drawLight() = 
+        let radius = 30.0
+        let pointPolar (x1, x2) (r, t) =
+            (x1 + r * cos t, x2 + r * sin t)
+        let coords = 
+            [0.0 .. 0.1 .. 2.0 * System.Double.Pi + 0.1] 
+            |> List.map (fun x -> pointPolar (float w/2.0, float h/2.0) (radius, x))
+        piecewiseAffine yellow 7.0 coords
+
     
 // Initialize multiple moths
 let moths = 
     [ for _ in 1..5 -> 
         Moth (GetRandomRange 0.0 (float w), GetRandomRange 0.0 (float h)) ]
 
+let light = Light
+
 // Combine multiple PrimitiveTrees into one using `onto`
 let drawAllMoths (mothList: Moth list) =
     mothList
     |> List.map (fun m -> m.draw())
     |> List.reduce (fun acc tree -> onto acc tree) // Combine all trees with `onto`
+
 
 let reactAllMoths (mothList: Moth list) =
     mothList |> List.iter (fun m -> m.nextPos())
@@ -60,7 +74,7 @@ let react (mothList: Moth list) (ev: Event) : Moth list option =
     | _ -> Some mothList
 
 let animation (mothList: Moth list) = 
-    make (drawAllMoths mothList)
+    make (onto light.drawLight (drawAllMoths mothList))
 
 // Run the simulation
 interact "Moth Simulation" w h (Some (1000 / 60)) animation react moths
