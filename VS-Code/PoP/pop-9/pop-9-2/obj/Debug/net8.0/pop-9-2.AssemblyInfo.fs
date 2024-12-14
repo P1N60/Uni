9e71217b339caabbe36acb08fac67478266fe755
P1N60 <victor.guldregn@gmail.com>
2024-12-14 15:37:50 +0100

    16

--- VS-Code/PoP/pop-9/pop-9-2/obj/Debug/net8.0/pop-9-2.AssemblyInfo.fs
+++ VS-Code/PoP/pop-9/pop-9-2/obj/Debug/net8.0/pop-9-2.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("pop-9-2")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+484680e4001567fc4f63e39d4f83381800eabc7e")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebcba77adab207b5c2bedbb11b40dc6917456ba")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("pop-9-2")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("pop-9-2")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
