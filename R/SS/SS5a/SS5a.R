74b924eafb880056d6641c50f094ed432ce0b8ea
Victor <victor.guldregn@gmail.com>
2024-12-15 18:25:06 +0100

    update 5 files and delete 1 file

--- R/SS/SS5a/SS5a.R
+++ R/SS/SS5a/SS5a.R
@@ -4,6 +4,15 @@ data <- transform(data, logPay=log(Pay))
 
 # 1. Opgave
 payMedian <- median(data$Pay)
+payAverage <- mean(data$Pay)
+paySampleVariance <- var(data$Pay)
+paySampleDeviation <- sd(data$Pay)
 logPayMedian <- median(data$logPay)
-payAverage <- sum(data$Pay)/length(data$Pay)
-logPayAverage <- sum(data$logPay)/length(data$logPay)
+logPayAverage <- mean(data$logPay)
+logPaySampleVariance <- var(data$logPay)
+logPaySampleDeviation <- sd(data$logPay)
+
+# 2. Opgave
+hist(data$Pay, prob=TRUE)
+
+hist(data$logPay, prob=TRUE)
