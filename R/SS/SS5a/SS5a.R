79675525e6135344e07e6345700107775a53dd6d
Victor <victor.guldregn@gmail.com>
2024-12-15 20:58:03 +0100

    update SS5a.R, HOF.assets.cache and image-classifier.ipynb

--- R/SS/SS5a/SS5a.R
+++ R/SS/SS5a/SS5a.R
@@ -14,5 +14,9 @@ logPaySampleDeviation <- sd(data$logPay)
 
 # 2. Opgave
 hist(data$Pay, prob=TRUE)
+f1 <- function(x) dnorm(x, mean=payAverage, sd=paySampleDeviation)
+curve(f1, add=TRUE, col="red", lwd=2.5)
 
 hist(data$logPay, prob=TRUE)
+f2 <- function(x) dnorm(x, mean=logPayAverage, sd=logPaySampleDeviation)
+curve(f2, add=TRUE, col="red", lwd=2.5)
