95086ba55524448c6ef00de43e71c8a279a4dd73
Victor <victor.guldregn@gmail.com>
2024-12-17 18:02:40 +0100

    update SS5a.R

--- R/SS/SS5a/SS5a.R
+++ R/SS/SS5a/SS5a.R
@@ -20,3 +20,16 @@ curve(f1, add=TRUE, col="red", lwd=2.5)
 hist(data$logPay, prob=TRUE)
 f2 <- function(x) dnorm(x, mean=logPayAverage, sd=logPaySampleDeviation)
 curve(f2, add=TRUE, col="red", lwd=2.5)
+
+# For Pay
+qqPay <- qqnorm(data$Pay, main="QQ-Plot af Pay")
+abline(payAverage, paySampleDeviation, col="red", lwd=2.2, lty=2)
+# For LogPay
+qqLogPay <- qqnorm(data$logPay, main="QQ-Plot af LogPay")
+abline(logPayAverage, logPaySampleDeviation, col="red", lwd=2.2, lty=2)
+
+# 3. Opgave
+# med normalfordeling
+integrate(f1, 100000, max(data$Pay))
+# uden normalfordelingsantagelse
+length(data$Pay[data$Pay>=100000])/length(data$Pay)
