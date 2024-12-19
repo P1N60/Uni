09ad9fb03d5e6594c6d07f6a706333830d817133
P1N60 <victor.guldregn@gmail.com>
2024-12-19 18:11:24 +0100

    update .Rhistory, SS5a.R and pop13-2.ipynb

--- R/SS/SS5a/SS5a.R
+++ R/SS/SS5a/SS5a.R
@@ -1,6 +1,6 @@
 setwd("~/Documents/GitHub/R/SS/SS5a")
 data <- read.table("paydata2017.txt", header=TRUE)
-data <- transform(data, logPay=log(Pay))
+data <- transform(data, logPay=log(data$Pay))
 
 # 1. Opgave
 payMedian <- median(data$Pay)
@@ -13,11 +13,11 @@ logPaySampleVariance <- var(data$logPay)
 logPaySampleDeviation <- sd(data$logPay)
 
 # 2. Opgave
-hist(data$Pay, prob=TRUE)
+hist(data$Pay, prob=TRUE, breaks=50)
 f1 <- function(x) dnorm(x, mean=payAverage, sd=paySampleDeviation)
 curve(f1, add=TRUE, col="red", lwd=2.5)
 
-hist(data$logPay, prob=TRUE)
+hist(data$logPay, prob=TRUE, breaks=50)
 f2 <- function(x) dnorm(x, mean=logPayAverage, sd=logPaySampleDeviation)
 curve(f2, add=TRUE, col="red", lwd=2.5)
 
