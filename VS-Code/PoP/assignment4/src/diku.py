66f10e8b762f497e53f505078946082a39f8482f
Victor <victor.guldregn@gmail.com>
2025-01-12 19:26:54 +0100

    update diku.py

--- VS-Code/PoP/assignment4/src/diku.py
+++ VS-Code/PoP/assignment4/src/diku.py
@@ -8,24 +8,62 @@ def lineReader(path):
 rows = lineReader("../data/diku.txt")
 
 def rowsToColumns():
+    "Converts list of letters in rows to list of letters in columns"
+
     result = []
     i = 0
+    for row in rows:
+        result.append("")
     while i < len(rows):
         for row in rows:
-            if i == 0:
-                result.append("")
-            result[i] += f"{row[i]}"
+            result[i] += row[i]
         i += 1
     return result
 
 columns = rowsToColumns()
-print(columns)
 
+def rowsToDiagonals():
+    "Converts list of letters in rows to list of letters in diagonals"
+
+    # Diagonal from right to left
+    result = []
+    j = 0
+    for row in rows:
+        result.append("")
+        i = 0
+        while i < len(row):
+            if j == 0 and i < len(row)-1:
+                result.append("")
+            result[j+i] += row[i]
+            i += 1
+        j += 1
+    j = 2*len(rows)-1
+    # Diagonal from left to right
+    for row in rows:
+        result.append("")
+        i = 0
+        while i < len(row):
+            if j == 2*len(row)-1 and i < len(row)-1:
+                result.append("")
+            result[j+i] += row[len(row)-i-1]
+            i += 1
+        j += 1
+    return result
+
+diagonals = rowsToDiagonals()
+
+# Count all points
 dikuCount = 0
 for row in rows:
+    # In rows as normal and backwards
     dikuCount += row.count("DIKU") + row.count("UKID")
 
 for column in columns:
+    # In columns as normal and backwards
     dikuCount += column.count("DIKU") + column.count("UKID")
+
+for diagonal in diagonals:
+    # In diagonals as normal and backwards
+    dikuCount += diagonal.count("DIKU") + diagonal.count("UKID")
 
 print(dikuCount)
