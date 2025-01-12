68bdc2afc406fd93c1882655be6813d4d8e7cb7b
Victor <victor.guldregn@gmail.com>
2025-01-12 16:10:45 +0100

    update diku.py

--- VS-Code/PoP/assignment4/src/diku.py
+++ VS-Code/PoP/assignment4/src/diku.py
@@ -6,4 +6,26 @@ def lineReader(path):
     return [line.strip() for line in lines]
 
 rows = lineReader("../data/diku.txt")
-print(rows)
+
+def rowsToColumns():
+    result = []
+    i = 0
+    while i < len(rows):
+        for row in rows:
+            if i == 0:
+                result.append("")
+            result[i] += f"{row[i]}"
+        i += 1
+    return result
+
+columns = rowsToColumns()
+print(columns)
+
+dikuCount = 0
+for row in rows:
+    dikuCount += row.count("DIKU") + row.count("UKID")
+
+for column in columns:
+    dikuCount += column.count("DIKU") + column.count("UKID")
+
+print(dikuCount)
