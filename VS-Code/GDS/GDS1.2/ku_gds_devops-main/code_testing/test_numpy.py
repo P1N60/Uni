af311cd102ed4d2e38f56384234d9f77631a471f
P1N60 <victor.guldregn@gmail.com>
2025-02-06 15:03:36 +0100

    add new tests for numpy operations and include wandb output files

--- VS-Code/GDS/GDS1.2/ku_gds_devops-main/code_testing/test_numpy.py
+++ VS-Code/GDS/GDS1.2/ku_gds_devops-main/code_testing/test_numpy.py
@@ -1,9 +1,16 @@
 import numpy as np
-import pytest
-
 
 def test_matmul():
     x = np.random.randn(5, 10)
     y = np.random.randn(10)
     xy = np.matmul(x, y)
     assert xy.shape == (5,)
+
+def test_int_add():
+    x = np.random.randint(0,10)
+    y = np.random.randint(1,10)
+    assert x+y != 0
+
+def test_np_sin():
+    assert np.sin(1) > 0
+    assert np.sin(0) == 0
--- VS-Code/GDS/GDS1.2/ku_gds_devops-main/experiment_tracking/wandb_script.py
+++ VS-Code/GDS/GDS1.2/ku_gds_devops-main/experiment_tracking/wandb_script.py
@@ -1,22 +1,29 @@
 import argparse
 
+import wandb
 from sklearn import datasets
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold, cross_val_score, train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
-models = {
-    "LogisticRegression": LogisticRegression(solver="liblinear", multi_class="ovr"),
-    "LinearDiscriminantAnalysis": LinearDiscriminantAnalysis(),
-    "KNeighborsClassifier": KNeighborsClassifier(),
+# Define sweep config
+sweep_configuration = {
+    "method": "random",
+    "name": "sweep",
+    "metric": {"goal": "maximize", "name": "accuracy"},
+    "parameters": {
+        "n_neighbors": {"min": 2, "max": 20},  # uniform sample value from 2 to 20
+        "weights": {"values": ["uniform", "distance"]},
+        "p": {"values": [1, 2, 3]},
+    },
 }
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--model", default="LogisticRegression", type=str)
-    args = parser.parse_args()
+def main():
+    run = wandb.init(project="iris-sweep")
+
+    n_neighbors = run.config.n_neighbors
+    weights = run.config.weights
+    p = run.config.p
 
     # Load dataset
     df = datasets.load_iris()
@@ -27,11 +34,18 @@ if __name__ == "__main__":
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1)
 
     # Instantiate the model
-    model = models[args.model]
-
-    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights, p=p)
 
     # Get metrics
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
     accuracy = cross_val_score(model, X_train, y_train, cv=kfold, scoring="accuracy").mean()
     f1_macro = cross_val_score(model, X_train, y_train, cv=kfold, scoring="f1_macro").mean()
     neg_log_loss = cross_val_score(model, X_train, y_train, cv=kfold, scoring="neg_log_loss").mean()
+
+    # Log the results
+    wandb.log({"accuracy": accuracy, "f1_macro": f1_macro, "neg_log_loss": neg_log_loss})
+
+
+if __name__ == "__main__":
+    sweep_id = wandb.sweep(sweep=sweep_configuration, project="iris-sweep")
+    wandb.agent(sweep_id, function=main, count=10)
