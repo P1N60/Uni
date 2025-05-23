b21ca6d17948833f5cb5dc2daf5e7d4b75ada692
P1N60 <victor.guldregn@gmail.com>
2025-05-23 15:57:16 +0200

    Add data and routines for testing AdvancedExtensions module in ProjectC
    - Introduced a new file `data_projectC.py` containing matrices, vectors, and expected results for various linear algebra operations.
    - Included multiple matrix sizes and configurations to facilitate comprehensive testing.
    - Organized data into lists for easy access during testing of the AdvancedExtensions module.
    - Ensured that the file is automatically generated and should not be modified manually.

---
+++ Uni/LaTeX/LinAlgDat/project_C/ProjectCPython/ProjectC/AdvancedExtensions.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+@Project: LinalgDat2022
+@File: AdvancedExtensions.py
+
+@Description: Project C Determinant and Gram-Schmidt extensions.
+
+"""
+
+import math
+from sys import path
+path.append('../Core')
+from Matrix import Matrix
+from Vector import Vector
+
+
+Tolerance = 1e-6
+
+
+def SquareSubMatrix(A: Matrix, i: int, j: int) -> Matrix:
+    """
+    This function creates the square submatrix given a square matrix as
+    well as row and column indices to remove from it.
+
+    Remarks:
+        See page 246-247 in "Linear Algebra for Engineers and Scientists"
+        by K. Hardy.
+
+    Parameters:
+        A:  N-by-N matrix
+        i: int. The index of the row to remove.
+        j: int. The index of the column to remove.
+
+    Return:
+        The resulting (N - 1)-by-(N - 1) submatrix.
+    """
+    raise NotImplementedError()
+
+
+def Determinant(A: Matrix) -> float:
+    """
+    This function computes the determinant of a given square matrix.
+
+    Remarks:
+        * See page 247 in "Linear Algebra for Engineers and Scientists"
+        by K. Hardy.
+        * Hint: Use SquareSubMatrix.
+
+    Parameter:
+        A: N-by-N matrix.
+
+    Return:
+        The determinant of the matrix.
+    """
+    raise NotImplementedError()
+
+
+def VectorNorm(v: Vector) -> float:
+    """
+    This function computes the Euclidean norm of a Vector. This has been implemented
+    in Project A and is provided here for convenience
+
+    Parameter:
+         v: Vector
+
+    Return:
+         Euclidean norm, i.e. (\sum v[i]^2)^0.5
+    """
+    nv = 0.0
+    for i in range(len(v)):
+        nv += v[i]**2
+    return math.sqrt(nv)
+
+
+def SetColumn(A: Matrix, v: Vector, j: int) -> Matrix:
+    """
+    This function copies Vector 'v' as a column of Matrix 'A'
+    at column position j.
+
+    Parameters:
+        A: M-by-N Matrix.
+        v: size M vector
+        j: int. Column number.
+
+    Return:
+        Matrix A  after modification.
+
+    Raise:
+        ValueError if j is out of range or if len(v) != A.M_Rows.
+    """
+    raise NotImplementedError()
+
+
+def GramSchmidt(A: Matrix) -> tuple:
+    """
+    This function computes the Gram-Schmidt process on a given matrix.
+
+    Remarks:
+        See page 229 in "Linear Algebra for Engineers and Scientists"
+        by K. Hardy.
+
+    Parameter:
+        A: M-by-N matrix. All columns are implicitly assumed linear
+        independent.
+
+    Return:
+        tuple (Q,R) where Q is a M-by-N orthonormal matrix and R is an
+        N-by-N upper triangular matrix.
+    """
+    raise NotImplementedError()
+
---
+++ Uni/LaTeX/LinAlgDat/project_C/ProjectCPython/ProjectC/README.md
@@ -0,0 +1,13 @@
+__The language settings are those of Python 3.6 or higher.__
+This should run on Windows, Linux and macOS if Python is installed properly (using for instance Anaconda or Miniconda). 
+
+
+* For Windows platforms, to execute your code, navigate to ProjectC folder and  
+  C:\FolderPath> python TestProjectC.py
+* For MacOS/Linux platforms, to execute your code, navigate to ProjectC folder and  
+ folder_path$ python TestProjectC.py
+
+#### Please note:
+__Vector.py con compute inner product as v @ w (as with numpy).__
+
+This code was written by François Lauze (francois@di.ku.dk).
