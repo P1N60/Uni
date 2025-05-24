9236ba41820c2784daae73df0e548b2ad48db1f8
P1N60 <victor.guldregn@gmail.com>
2025-05-24 12:17:45 +0200

    Implement Gram-Schmidt process and related vector operations in AdvancedExtensions module

--- Uni/LaTeX/LinAlgDat/project_C/ProjectCPython/ProjectC/AdvancedExtensions.py
+++ Uni/LaTeX/LinAlgDat/project_C/ProjectCPython/ProjectC/AdvancedExtensions.py
@@ -45,6 +45,7 @@ def SquareSubMatrix(A: Matrix, i: int, j: int) -> Matrix:
             # skip the column if it is the column we want to remove
             if c == j:
                 continue
+            # add the element to the correct position
             if r > i and c > j:
                 B[r - 1, c - 1] = A[r, c]
             elif r > i:
@@ -71,13 +72,14 @@ def Determinant(A: Matrix) -> float:
         The determinant of the matrix.
     """
     N = A.N_Cols
+    # if the squaresubmatrix is of size 1, then return that element
     if N == 1:
         return A[0, 0]
     result = 0
+    # formula for calculating the determinant recursively
     for j in range(N):
         result += A[0, j] * ((-1) ** (1 + j)) * Determinant(SquareSubMatrix(A, 0, j))
     return result
-
 
 def VectorNorm(v: Vector) -> float:
     """
@@ -95,7 +97,6 @@ def VectorNorm(v: Vector) -> float:
         nv += v[i]**2
     return math.sqrt(nv)
 
-
 def SetColumn(A: Matrix, v: Vector, j: int) -> Matrix:
     """
     This function copies Vector 'v' as a column of Matrix 'A'
@@ -112,8 +113,32 @@ def SetColumn(A: Matrix, v: Vector, j: int) -> Matrix:
     Raise:
         ValueError if j is out of range or if len(v) != A.M_Rows.
     """
-    raise NotImplementedError()
+    M = A.M_Rows
+    N = A.N_Cols
+    n = v.__len__()
+    # raise error if j is out of range or if v.__len__() != A.M_Rows.
+    if j < 0 or j >= N or n != M:
+        raise ValueError 
+    for r in range(M):
+        for c in range(N):
+            # copy the elements of the vector into A if this is the j'th column
+            if c == j:
+                A[r, c] = v[r]
+    return A
 
+def extract_vector(A: Matrix, j: int) -> Vector:
+    M = A.M_Rows
+    N = A.N_Cols
+    v = Vector(M)
+    for i in range(M):
+        v[i] = A[i, j]
+    return v
+
+def vector_dot_product(v: Vector, y: Vector) -> float:
+    result = 0
+    for i in range(v.__len__()):
+        result += v[i] * y[i]
+    return result
 
 def GramSchmidt(A: Matrix) -> tuple:
     """
@@ -131,5 +156,20 @@ def GramSchmidt(A: Matrix) -> tuple:
         tuple (Q,R) where Q is a M-by-N orthonormal matrix and R is an
         N-by-N upper triangular matrix.
     """
-    raise NotImplementedError()
-
+    M = A.M_Rows
+    N = A.N_Cols
+    Q = Matrix(M, N)
+    R = Matrix(N, N)
+    for j in range(N):
+        v = extract_vector(A, j)
+        q = Vector(M)
+        for i in range(j):
+            qi = extract_vector(Q, i)
+            R[i, j] = vector_dot_product(qi, v)
+            for k in range(M):
+                v[k] += -R[i, j] * qi[k]
+        R[j, j] = VectorNorm(v)
+        for k in range(M):
+            q[k] = v[k] / R[j, j]
+        SetColumn(Q, q, j)
+    return (Q, R)
